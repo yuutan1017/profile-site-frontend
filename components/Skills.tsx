ae6e3964416433d53f4609b9b974534677a406a7
yuutan1017 <n.you0611@gmail.com>
2022-10-10 19:35:27 +0900

    ok

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,32 +1,83 @@
-import React from "react";
-import { Progress, Text } from "@mantine/core";
-import { SkillProps } from "../types/type";
+import React, { useRef } from "react";
+import { Center, Progress, Stack, Text } from "@mantine/core";
+import { Carousel } from "@mantine/carousel";
+import Autoplay from "embla-carousel-autoplay";
+
+import type { SkillProps } from "../types/type";
 
 const SkillBar = (props: SkillProps) => {
   return (
     <>
       <Text size="lg">{props.label}</Text>
       <Progress
-        size="lg"
+        size="md"
         sections={[{ value: props.value, color: props.color }]}
-        radius="lg"
+        radius="sm"
       />
     </>
   );
 };
 
+
 export const Skills = () => {
+  const autoplay = useRef(Autoplay({ delay: 4000 }));
   return (
     <div className="mt-12">
-      <div className="mb-8 text-2xl">
+      <div className="mb-8 text-3xl">
         <Text>|| skills</Text>
       </div>
-      <div className="mt-4">
-        <SkillBar label="JavaScript" value={72} color="#f1e05a" />
-      </div>
-      <div className="mt-4">
-        <SkillBar label="TypeScript" value={72} color="#2b7489" />
-      </div>
+      <Carousel
+        sx={{ maxWidth: 520 }}
+        mx="auto"
+        slideGap="lg"
+        loop
+        withControls={false}
+        height={350}
+        plugins={[autoplay.current]}
+        onMouseEnter={autoplay.current.stop}
+        onMouseLeave={autoplay.current.reset}
+      >
+        <Carousel.Slide>
+          <Center>
+            <Text size="xl" align="justify">
+              Front End
+            </Text>
+          </Center>
+          <Stack spacing="sm">
+            <SkillBar label="HTML" value={75} color="#e44b23" />
+            <SkillBar label="CSS" value={70} color="#563d7c" />
+            <SkillBar label="JavaScript" value={72} color="#f1e05a" />
+            <SkillBar label="TypeScript" value={72} color="#2b7489" />
+          </Stack>
+        </Carousel.Slide>
+
+        <Carousel.Slide>
+          <Center>
+            <Text size="xl" align="justify">
+              Back End
+            </Text>
+          </Center>
+          <Stack spacing="sm">
+            <SkillBar label="Java" value={65} color="#b07219" />
+            <SkillBar label="Python" value={70} color="#3572A5" />
+            <SkillBar label="PHP" value={55} color="#4F5D95" />
+            <SkillBar label="Ruby" value={50} color="#701516" />
+          </Stack>
+        </Carousel.Slide>
+
+        <Carousel.Slide>
+          <Center>
+            <Text size="xl" align="justify">
+              DevOps
+            </Text>
+          </Center>
+          <Stack spacing="sm">
+            <SkillBar label="Git" value={70} color="#f0f0e8" />
+            <SkillBar label="AWS" value={60} color="#232f3e" />
+            <SkillBar label="Docker" value={60} color="#2496ed" />
+          </Stack>
+        </Carousel.Slide>
+      </Carousel>
     </div>
   );
 };
