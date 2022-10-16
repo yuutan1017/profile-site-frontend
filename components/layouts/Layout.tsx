2f05b6b2c485ac960d5022683cdb7f47a7669c8b
yuutan1017 <n.you0611@gmail.com>
2022-10-16 18:51:58 +0900

    build

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -19,7 +19,7 @@ export const Layout = ({ children }: any): JSX.Element => {
   return (
     <div className="2xl:mx-96 xl:mx-64 lg:mx-48 md:mx-24 sm:px-10 mx-5 mt-5 ">
       <HeaderResponsive />
-      {/* <Material /> */}
+      <Material />
       <main className="min-h-screen sm:mx-24 mx-3">{children}</main>
       <Footer />
     </div>
