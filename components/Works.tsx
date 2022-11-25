fdb3b579cd0d7e996985623e7eb70179942a3cd4
yuutan1017 <n.you0611@gmail.com>
2022-11-25 14:19:59 +0900

    fix:workd carousel

--- components/Works.tsx
+++ components/Works.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
-import { SimpleGrid, Card, Text, Container } from "@mantine/core";
+import { Card, Text } from "@mantine/core";
 import { Carousel } from "@mantine/carousel";
 
 import { useStyles } from "./layouts/style";
@@ -39,50 +39,58 @@ const data = [
 export function Works() {
   const { classes, cx } = useStyles();
   const cards = data.map((article: any) => (
-    <Card
-      key={article.title}
-      p="md"
-      target="_blank"
-      radius="lg"
-      component={article.component}
-      href={article.href}
-      className="sm:w-2/5 w-80 border sm:mx-3 mx-1"
-    >
-      <Image src={article.image} alt={article.alt} width="350" height="200" />
-      <Text mt={16} mb={16} weight={600}>
-        {article.title}
-      </Text>
-      <Text size="sm" color="dimmed">
-        {article.description}
-      </Text>
-      {article.href ? (
-        <div className="mt-6 hover:text-blue-500">
-          <Link href={article.href} passHref>
-            <a>デモページはこちら</a>
-          </Link>
-        </div>
-      ) : (
-        <></>
-      )}
-    </Card>
+    <Carousel.Slide className="flex justify-center items-center my-4">
+      <Card
+        key={article.title}
+        p="md"
+        target="_blank"
+        radius="lg"
+        component={article.component}
+        href={article.href}
+        className="w-80 border shadow-xl sm:mx-3 mx-1"
+      >
+        <Image
+          src={article.image}
+          alt={article.alt}
+          width="350"
+          height="200"
+          className="rounded-md"
+        />
+        <Text mt={16} mb={16} weight={600}>
+          {article.title}
+        </Text>
+        <Text size="sm" color="dimmed">
+          {article.description}
+        </Text>
+        {article.href ? (
+          <div className="mt-6 hover:text-blue-500">
+            <Link href={article.href} passHref>
+              <a>デモページはこちら</a>
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </Card>
+    </Carousel.Slide>
   ));
 
   return (
-    <motion.div variants={works} initial="hidden" animate="visible">
+    <motion.div className="mb-10" variants={works} initial="hidden" animate="visible">
       <div className="mb-10 mt-20">
         <Text className={cx(classes.chapter)} weight={600}>
           Works
         </Text>
-      </div> 
+      </div>
       <Carousel
-        className="sm:px-6"
+        withIndicators
         loop
-        breakpoints={[{ maxWidth: "sm", slideSize: "100%", slideGap: 1 }]}
+        breakpoints={[{ maxWidth: "sm", slideSize: "100%", slideGap: 2 }]}
         slideGap="xl"
         mx="auto"
         align="start"
       >
-        <Carousel.Slide className="flex">{cards}</Carousel.Slide>
+        {cards}
       </Carousel>
     </motion.div>
   );
